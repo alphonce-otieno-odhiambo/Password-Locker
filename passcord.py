10e313022cc36ff737525304ee7190f66d5b9ccc
alphonce-otieno-odhiambo <alphonce.odhiambo@student.moringaschool.com>
2021-10-25 23:08:40 +0300

    create a save method

--- passcord.py
+++ passcord.py
@@ -7,8 +7,14 @@ class User:
     user_list_in = []
     '''user list for storing User objects'''
 
-    '''create an instance of class'''
+    
     def __innit__(self,name, passcord ):
+        '''create an instance of class'''
         self.name = name
         self.passcord = passcord
+        
+    
+    def save_user(self):
+    '''create a methord for saving the object'''
+        User.user_list_in.append(self)
 
