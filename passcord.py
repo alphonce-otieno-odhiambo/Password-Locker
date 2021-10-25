f27442760ba9794f48e727383581524236589daa
alphonce-otieno-odhiambo <alphonce.odhiambo@student.moringaschool.com>
2021-10-25 22:56:19 +0300

    create the objects

--- passcord.py
+++ passcord.py
@@ -6,4 +6,9 @@ class User:
     '''user class for creating class instances'''
     user_list_in = []
     '''user list for storing User objects'''
-    def __innit__(self,name, passcord )
+
+    '''create an instance of class'''
+    def __innit__(self,name, passcord ):
+        self.name = name
+        self.passcord = passcord
+
