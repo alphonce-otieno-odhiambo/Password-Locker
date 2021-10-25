d0d99a1f42dcd7fe2b5511d228ccb156dfebf15e
alphonce-otieno-odhiambo <alphonce.odhiambo@student.moringaschool.com>
2021-10-26 01:03:19 +0300

    generate a new file and importation

---
+++ run.py
@@ -0,0 +1,7 @@
+import pyperclip
+from passcord import User, Creadentials
+
+def creat_user(name, ppasscord):
+    '''creates a new user account'''
+    new_user_user = User(name, passcord)
+    return new_user_user
