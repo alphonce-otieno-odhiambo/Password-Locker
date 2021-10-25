24482a6f700a67e3c90e834c2f425bb071d1a9c5
alphonce-otieno-odhiambo <alphonce.odhiambo@student.moringaschool.com>
2021-10-25 22:52:42 +0300

    creat class  User  with the instances

--- passcord.py
+++ passcord.py
@@ -1,2 +1,9 @@
 import random
 import pyperclip
+'''import the modules '''
+class User:
+    
+    '''user class for creating class instances'''
+    user_list_in = []
+    '''user list for storing User objects'''
+    def __innit__(self,name, passcord )
